fc2ab80656c1e703b18bc6a84869ad44b00b140e
Valium-K <lschan96@gmail.com>
2020-10-27 15:11:01 +0900

    Cookie사용

--- sp5-chap13/src/main/java/controller/LoginController.java
+++ sp5-chap13/src/main/java/controller/LoginController.java
@@ -2,14 +2,13 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import spring.AuthInfo;
 import spring.AuthService;
 import spring.WrongIdPasswordException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 // 로그인 요청 처리 컨트롤러
@@ -23,12 +22,23 @@ public class LoginController {
     }
 
     @GetMapping
-    public String form(LoginCommand loginCommand) {
+    public String form(LoginCommand loginCommand,
+                       // 이름이 REMEMBER인 쿠키를 cOOKIE 타입으로 전달받는다.
+                       // 이 때 지정한 이름의 쿠키가 존재하지 않을 수 있다면 required = false이다. (기본값은 true)
+                       @CookieValue(value = "REMEMBER", required = false) Cookie rCookie) {
+
+        // REMEBER쿠키가 존재하면
+        if(rCookie != null) {
+            loginCommand.setEmail(rCookie.getValue());
+            loginCommand.setRememberEmail(true);
+        }
         return "login/loginForm";
     }
 
     @PostMapping
-    public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
+    public String submit(LoginCommand loginCommand, Errors errors, HttpSession session,
+                         // 쿠키를 생성하기 위한 response
+                         HttpServletResponse response) {
         new LoginCommandValidator().validate(loginCommand, errors);
         if(errors.hasErrors())
             return "login/loginForm";
@@ -38,6 +48,21 @@ public class LoginController {
 
             // 로그인 성공 시 HttpSession의 "authInfo"속성에 authInfo객체를 저장.
             session.setAttribute("authInfo", authInfo);
+
+
+            Cookie rememberCookie = new Cookie("REMEMBER", loginCommand.getEmail());
+            rememberCookie.setPath("/");
+
+            if(loginCommand.isRememberEmail()) {
+                // 1분 유지
+                rememberCookie.setMaxAge(60);
+            }
+            else {
+                // 파기
+                rememberCookie.setMaxAge(0);
+            }
+            response.addCookie(rememberCookie);
+
             return "login/loginSuccess";
         } catch (WrongIdPasswordException e) {
             errors.reject("idPasswordNotMatching");
